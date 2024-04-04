6a052456a3a9ff445a3282050a39e7296305f924
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-04-04 10:44:46 +0200

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -57,6 +57,32 @@ GMAIL_ADDRESS = st.secrets["GMAIL_ADDRESS"]
 GMAIL_PASSWORD = st.secrets["GMAIL_PASSWORD"]
 TO_ADRESS_EMAIL = st.secrets["TO_ADRESS_EMAIL"]
 
+# Météo
+def get_meteo_data():
+    global DJ_TEMPERATURE_REFERENCE
+    # Mise à jour des données météo de manière journalière
+    df_meteo_tre200d0_historique = pd.read_csv("https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_GVE_previous.csv", sep=';', encoding='latin1', parse_dates=['date'])
+    df_meteo_tre200d0_annee_cours = pd.read_csv("https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_GVE_current.csv", sep=';', encoding='latin1', parse_dates=['date'])
+    df_meteo_tre200d0 = pd.concat([df_meteo_tre200d0_historique, df_meteo_tre200d0_annee_cours])
+    df_meteo_tre200d0.rename(columns={"station/location": "stn",
+                        'date': 'time'}, inplace=True)
+    df_meteo_tre200d0 = df_meteo_tre200d0[['stn', 'time', 'tre200d0']]
+    df_meteo_tre200d0 = df_meteo_tre200d0[df_meteo_tre200d0['time'] >= '2015-01-01']
+    df_meteo_tre200d0.drop_duplicates(inplace=True)
+    # replace values with "-" with nan and drop them
+    df_meteo_tre200d0.replace("-", pd.NA, inplace=True)
+    df_meteo_tre200d0.dropna(inplace=True)
+    df_meteo_tre200d0.sort_values(by="time", inplace=True)
+    df_meteo_tre200d0.reset_index(drop=True, inplace=True)
+    # Calculs pour DJ
+    df_meteo_tre200d0['tre200d0'] = df_meteo_tre200d0['tre200d0'].astype(float)
+    df_meteo_tre200d0['mois'] = df_meteo_tre200d0['time'].dt.month
+    df_meteo_tre200d0['saison_chauffe'] = np.where(df_meteo_tre200d0['mois'].isin([9, 10, 11, 12, 1, 2, 3, 4, 5]), 1, 0)
+    df_meteo_tre200d0['tre200d0_sous_16'] = np.where(df_meteo_tre200d0['tre200d0'] <= 16, 1, 0)
+    df_meteo_tre200d0['DJ_theta0_16'] = df_meteo_tre200d0['saison_chauffe'] * df_meteo_tre200d0['tre200d0_sous_16'] * (DJ_TEMPERATURE_REFERENCE - df_meteo_tre200d0['tre200d0'])
+
+    return df_meteo_tre200d0
+
 def generate_dashboard():
     # Validation données saisies
     def validate_input(name, variable, unité):
@@ -64,32 +90,6 @@ def generate_dashboard():
             st.text(f"{name} {variable} {unité}")
         else:
             st.text(name, "doit être un chiffre")
-    
-    # Météo
-    def get_meteo_data():
-        global DJ_TEMPERATURE_REFERENCE
-        # Mise à jour des données météo de manière journalière
-        df_meteo_tre200d0_historique = pd.read_csv("https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_GVE_previous.csv", sep=';', encoding='latin1', parse_dates=['date'])
-        df_meteo_tre200d0_annee_cours = pd.read_csv("https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_GVE_current.csv", sep=';', encoding='latin1', parse_dates=['date'])
-        df_meteo_tre200d0 = pd.concat([df_meteo_tre200d0_historique, df_meteo_tre200d0_annee_cours])
-        df_meteo_tre200d0.rename(columns={"station/location": "stn",
-                            'date': 'time'}, inplace=True)
-        df_meteo_tre200d0 = df_meteo_tre200d0[['stn', 'time', 'tre200d0']]
-        df_meteo_tre200d0 = df_meteo_tre200d0[df_meteo_tre200d0['time'] >= '2015-01-01']
-        df_meteo_tre200d0.drop_duplicates(inplace=True)
-        # replace values with "-" with nan and drop them
-        df_meteo_tre200d0.replace("-", pd.NA, inplace=True)
-        df_meteo_tre200d0.dropna(inplace=True)
-        df_meteo_tre200d0.sort_values(by="time", inplace=True)
-        df_meteo_tre200d0.reset_index(drop=True, inplace=True)
-        # Calculs pour DJ
-        df_meteo_tre200d0['tre200d0'] = df_meteo_tre200d0['tre200d0'].astype(float)
-        df_meteo_tre200d0['mois'] = df_meteo_tre200d0['time'].dt.month
-        df_meteo_tre200d0['saison_chauffe'] = np.where(df_meteo_tre200d0['mois'].isin([9, 10, 11, 12, 1, 2, 3, 4, 5]), 1, 0)
-        df_meteo_tre200d0['tre200d0_sous_16'] = np.where(df_meteo_tre200d0['tre200d0'] <= 16, 1, 0)
-        df_meteo_tre200d0['DJ_theta0_16'] = df_meteo_tre200d0['saison_chauffe'] * df_meteo_tre200d0['tre200d0_sous_16'] * (DJ_TEMPERATURE_REFERENCE - df_meteo_tre200d0['tre200d0'])
-
-        return df_meteo_tre200d0
 
     # Calcul des degrés-jours
     def calcul_dj_periode(df_meteo_tre200d0, periode_start, periode_end):
@@ -1207,6 +1207,9 @@ if __name__ == "__main__":
     st.session_state.GMAIL_ADDRESS = GMAIL_ADDRESS
     st.session_state.GMAIL_PASSWORD = GMAIL_PASSWORD
     st.session_state.TO_ADRESS_EMAIL = TO_ADRESS_EMAIL
+    # Mise à jour météo
+    df_meteo_tre200d0 = get_meteo_data()
+    st.session_state.df_meteo_tre200d0 = df_meteo_tre200d0
     # Titre de la page
     st.set_page_config(page_title="AMOEN Dashboard", page_icon=":bar_chart:", layout="wide")
     generate_dashboard()

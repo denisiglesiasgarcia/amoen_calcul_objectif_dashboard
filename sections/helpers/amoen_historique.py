1c6e5012e5db992b284600a2e6f2a5876c3507a1
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-08-08 14:15:06 +0200

    Update
    Ajout historique, version par site par amoen, ajout rapport, site spécial avusy

---
+++ sections/helpers/amoen_historique.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.express as px
+from datetime import datetime
+import streamlit as st
+
+@st.cache_data
+def create_barplot_historique_amoen(data_df):
+    df_barplot = data_df.copy()
+    df_barplot = df_barplot[['nom_projet', 'periode_start', 'periode_end', 'atteinte_objectif']].sort_values(by=['periode_start'])
+    df_barplot['periode'] = df_barplot['periode_start'].dt.strftime('%Y-%m-%d') + ' → ' + df_barplot['periode_end'].dt.strftime('%Y-%m-%d')
+    df_barplot['atteinte_objectif'] = df_barplot['atteinte_objectif'] * 100
+
+    nom_projet = df_barplot['nom_projet'][0]
+    
+    fig = px.bar(df_barplot, x='periode', y='atteinte_objectif',
+                 labels={'periode': 'Période', 'atteinte_objectif': "Atteinte de l'objectif [%]"},
+                 title=f"Historique atteinte objectifs pour {nom_projet}")
+    
+    # Customize the layout
+    fig.update_layout(
+        xaxis_title='Période',
+        yaxis_title="Atteinte de l'objectif [%]",
+        xaxis={'type': 'category'},  # Treat year as categorical
+        yaxis_range=[0, max(df_barplot['atteinte_objectif']) * 1.1],  # Extend y-axis range
+        margin=dict(t=100, b=100),  # Increase top and bottom margins
+        height=600  # Increase overall height of the plot
+    )
+    
+    # Add annotations for the values over the bars
+    fig.update_traces(texttemplate='%{y:.1f}%', textposition='outside')
+   
+    return fig

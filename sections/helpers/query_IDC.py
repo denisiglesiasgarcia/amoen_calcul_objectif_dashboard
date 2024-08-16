9e2848b2435a69bbc1066bf05867b1f8223f5a68
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-08-16 14:00:52 +0200

    Update query_IDC.py

--- sections/helpers/query_IDC.py
+++ sections/helpers/query_IDC.py
@@ -9,6 +9,7 @@ import pydeck as pdk
 import sqlite3
 import plotly.express as px
 from datetime import datetime
+import json
 
 @st.cache_data
 def make_request(offset: int, fields: str, url: str, chunk_size: int, table_name: str, geometry: bool, egid: Union[int, List[int]]) -> Optional[List[Dict]]:

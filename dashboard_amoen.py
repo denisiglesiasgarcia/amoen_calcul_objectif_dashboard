61bd414f1c5af4ea7601d7144466b5e194bfc66b
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-09-03 14:43:50 +0200

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -2542,6 +2542,8 @@ if st.session_state["authentication_status"]:
                     "sre_pourcentage_piscines_couvertes",
                 ]
             )
+            st.selectbox("Nom du projet", df["nom_projet"].unique())
+            st.selectbox("AMOén", df["amoen_id"].unique())
             st.write(df)
 
             # Add your content for the admin tab here

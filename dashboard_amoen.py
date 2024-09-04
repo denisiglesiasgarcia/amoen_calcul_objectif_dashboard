6e9db1d6dade218469087aa8c4dd306ba2fae85b
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-09-04 12:58:08 +0200

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -2561,6 +2561,7 @@ if st.session_state["authentication_status"]:
             ]
             # date dernier rapport par projet
             df_date_sorted = df_date.sort_values(["nom_projet", "date_rapport"])
+            df_date_sorted["date_rapport"] = df_date_sorted["date_rapport"].date()
             idx = df_date_sorted.groupby("nom_projet")["date_rapport"].idxmax()
             df_date = df_date_sorted.loc[
                 idx, ["nom_projet", "date_rapport", "periode_start", "periode_end"]

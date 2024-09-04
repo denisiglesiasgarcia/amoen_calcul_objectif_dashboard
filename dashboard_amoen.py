88207037f139ab4ba1e93be5fd2a678b30af9c5c
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-09-04 12:22:46 +0200

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -2630,7 +2630,7 @@ if st.session_state["authentication_status"]:
             # Add text labels on top of the bars
             text = (
                 alt.Chart(df_barplot)
-                .mark_text(align="left", baseline="middle", fontSize=14, color="black")
+                .mark_text(align="center", baseline="bottom", fontSize=14, color="black")
                 .encode(
                     x=alt.X("nom_projet:N", axis=alt.Axis(title="", labels=True)),
                     y=alt.Y("atteinte_objectif:Q", title="Atteinte Objectif [%]"),

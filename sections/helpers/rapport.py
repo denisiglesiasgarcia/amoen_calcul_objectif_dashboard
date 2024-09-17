9c907a9fe62da38499f77d8ef547a7aba74db61e
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-09-17 17:43:43 +0200

    Update rapport.py

--- sections/helpers/rapport.py
+++ sections/helpers/rapport.py
@@ -1005,7 +1005,7 @@ def generate_pdf(data):
             Image(
                 "02_reno_sur.png",
                 width=width_02_reno_sur,
-                height=(ratio_figsize_rep_reno_sur + 0.5) * width_02_reno_sur,
+                height=(ratio_figsize_rep_reno_sur + 0.55) * width_02_reno_sur,
             )
         )
     # elements.append(PageBreak())

30a70f04638a0b0021a42604a270da347d6dc462
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-10-01 18:31:42 +0200

    doc python

--- sections/helpers/query_IDC.py
+++ sections/helpers/query_IDC.py
@@ -14,7 +14,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def make_request(offset: int, fields: str, url: str, chunk_size: int, table_name: str, geometry: bool, egid: Union[int, List[int]]) -> Optional[List[Dict]]:
+
+def make_request(
+    offset: int,
+    fields: str,
+    url: str,
+    chunk_size: int,
+    table_name: str,
+    geometry: bool,
+    egid: Union[int, List[int]],
+) -> Optional[List[Dict]]:
     """
     Make an API request to retrieve data for one or multiple EGIDs.
     Args:
@@ -34,62 +43,102 @@ def make_request(offset: int, fields: str, url: str, chunk_size: int, table_name
     else:
         where_clause = f"egid={egid}"
     params = {
-        'where': where_clause,
-        'outFields': fields,
-        'returnGeometry': str(geometry).lower(),
-        'f': 'json',
-        'resultOffset': offset,
-        'resultRecordCount': chunk_size
+        "where": where_clause,
+        "outFields": fields,
+        "returnGeometry": str(geometry).lower(),
+        "f": "json",
+        "resultOffset": offset,
+        "resultRecordCount": chunk_size,
     }
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
-        if 'features' in data:
-            data_df = data['features']
+        if "features" in data:
+            data_df = data["features"]
             if geometry:
-                result = [{'attributes': d['attributes'], 'geometry': d['geometry']} for d in data_df]
+                result = [
+                    {"attributes": d["attributes"], "geometry": d["geometry"]}
+                    for d in data_df
+                ]
                 return result
             else:
-                result = [d['attributes'] for d in data_df]
+                result = [d["attributes"] for d in data_df]
 
                 df = pd.DataFrame(result)
 
-                df = df[[
-                        'egid', 'annee', 'indice', 'sre',
-                        'adresse', 'npa', 'commune', 'destination',
-                        'agent_energetique_1', 'quantite_agent_energetique_1', 'unite_agent_energetique_1',
-                        'agent_energetique_2', 'quantite_agent_energetique_2', 'unite_agent_energetique_2',
-                        'agent_energetique_3', 'quantite_agent_energetique_3', 'unite_agent_energetique_3',
-                        'date_debut_periode', 'date_fin_periode', 'date_saisie',
-                        'indice_moy2', 'annees_concernees_moy_2', 'indice_moy3', 'annees_concernees_moy_3',
-                        'id_concessionnaire', 'nbre_preneur',
-                        ]].sort_values(by=['egid', 'annee'])
+                df = df[
+                    [
+                        "egid",
+                        "annee",
+                        "indice",
+                        "sre",
+                        "adresse",
+                        "npa",
+                        "commune",
+                        "destination",
+                        "agent_energetique_1",
+                        "quantite_agent_energetique_1",
+                        "unite_agent_energetique_1",
+                        "agent_energetique_2",
+                        "quantite_agent_energetique_2",
+                        "unite_agent_energetique_2",
+                        "agent_energetique_3",
+                        "quantite_agent_energetique_3",
+                        "unite_agent_energetique_3",
+                        "date_debut_periode",
+                        "date_fin_periode",
+                        "date_saisie",
+                        "indice_moy2",
+                        "annees_concernees_moy_2",
+                        "indice_moy3",
+                        "annees_concernees_moy_3",
+                        "id_concessionnaire",
+                        "nbre_preneur",
+                    ]
+                ].sort_values(by=["egid", "annee"])
 
-                df['date_debut_periode'] = pd.to_datetime(df['date_debut_periode'], unit='ms')
-                df['date_fin_periode'] = pd.to_datetime(df['date_fin_periode'], unit='ms')
-                df['date_saisie'] = pd.to_datetime(df['date_saisie'], unit='ms')
+                df["date_debut_periode"] = pd.to_datetime(
+                    df["date_debut_periode"], unit="ms"
+                )
+                df["date_fin_periode"] = pd.to_datetime(
+                    df["date_fin_periode"], unit="ms"
+                )
+                df["date_saisie"] = pd.to_datetime(df["date_saisie"], unit="ms")
 
-                df['npa'] = df['npa'].astype('int64')
-                df['quantite_agent_energetique_1'] = df['quantite_agent_energetique_1'].astype('float64')
-                df['quantite_agent_energetique_2'] = df['quantite_agent_energetique_2'].astype('float64')
-                df['quantite_agent_energetique_3'] = df['quantite_agent_energetique_3'].astype('float64')
+                df["npa"] = df["npa"].astype("int64")
+                df["quantite_agent_energetique_1"] = df[
+                    "quantite_agent_energetique_1"
+                ].astype("float64")
+                df["quantite_agent_energetique_2"] = df[
+                    "quantite_agent_energetique_2"
+                ].astype("float64")
+                df["quantite_agent_energetique_3"] = df[
+                    "quantite_agent_energetique_3"
+                ].astype("float64")
 
                 # for each pair of egid and annee, keep only the moste recent date_saisie
-                df = df.sort_values(by=['egid', 'annee', 'date_saisie'], ascending=[True, True, False])
-                df = df.drop_duplicates(subset=['egid', 'annee'], keep='first')
+                df = df.sort_values(
+                    by=["egid", "annee", "date_saisie"], ascending=[True, True, False]
+                )
+                df = df.drop_duplicates(subset=["egid", "annee"], keep="first")
 
                 # convert dataframe to list of dictionaries
-                filtered_list = df.to_dict('records')
+                filtered_list = df.to_dict("records")
 
                 return filtered_list
         else:
-            logging.warning(f"{table_name} → 'features' key not found in the API response for offset {offset}")
+            logging.warning(
+                f"{table_name} → 'features' key not found in the API response for offset {offset}"
+            )
     except requests.exceptions.RequestException as e:
-        logging.error(f'{table_name} → An error occurred with {offset}: {e}')
+        logging.error(f"{table_name} → An error occurred with {offset}: {e}")
     except json.JSONDecodeError:
-        logging.error(f'{table_name} → An error occurred with {offset}, retrying later...')
+        logging.error(
+            f"{table_name} → An error occurred with {offset}, retrying later..."
+        )
     return None
+
 
 @st.cache_data
 def convert_geometry_for_streamlit(data: List[Dict]) -> tuple:
@@ -102,8 +151,8 @@ def convert_geometry_for_streamlit(data: List[Dict]) -> tuple:
     all_points = []
 
     for item in data:
-        if 'geometry' in item and 'rings' in item['geometry']:
-            rings = item['geometry']['rings']
+        if "geometry" in item and "rings" in item["geometry"]:
+            rings = item["geometry"]["rings"]
             new_rings = []
             for ring in rings:
                 new_ring = []
@@ -113,33 +162,28 @@ def convert_geometry_for_streamlit(data: List[Dict]) -> tuple:
                     new_ring.append([lon, lat])
                     all_points.append([lon, lat])
                 new_rings.append(new_ring)
-            
+
             feature = {
                 "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": new_rings
-                },
-                "properties": item['attributes']
+                "geometry": {"type": "Polygon", "coordinates": new_rings},
+                "properties": item["attributes"],
             }
             features.append(feature)
 
-    geojson = {
-        "type": "FeatureCollection",
-        "features": features
-    }
+    geojson = {"type": "FeatureCollection", "features": features}
 
     # Calculate the centroid of all points
     centroid = np.mean(all_points, axis=0)
 
     return geojson, centroid
 
+
 @st.cache_data
 def show_map(data: List[Dict], centroid: Tuple[float, float]) -> None:
     """
     Display a map with the given data and centroid.
     """
-    
+
     # Create a PyDeck layer
     layer = pdk.Layer(
         "GeoJsonLayer",
@@ -156,7 +200,7 @@ def show_map(data: List[Dict], centroid: Tuple[float, float]) -> None:
         auto_highlight=True,
         get_tooltip=["properties.egid", "properties.adresse"],
     )
-    
+
     # Set the initial view state using the calculated centroid
     view_state = pdk.ViewState(
         latitude=centroid[1],  # Latitude
@@ -164,7 +208,7 @@ def show_map(data: List[Dict], centroid: Tuple[float, float]) -> None:
         zoom=17,  # You might need to adjust this depending on the scale of your data
         pitch=45,
     )
-    
+
     # Create the deck
     deck = pdk.Deck(
         layers=[layer],
@@ -172,80 +216,119 @@ def show_map(data: List[Dict], centroid: Tuple[float, float]) -> None:
         map_style="mapbox://styles/mapbox/light-v9",
         tooltip={
             "html": "<b>EGID:</b> {egid}<br/><b>Adresse:</b> {adresse}<br/><b>SRE:</b> {sre} m",
-            "style": {
-                "backgroundColor": "steelblue",
-                "color": "white"
-            }
-        }
+            "style": {"backgroundColor": "steelblue", "color": "white"},
+        },
     )
-    
+
     # Display the map in Streamlit
     st.pydeck_chart(deck)
+
 
 @st.cache_data
 def show_dataframe(df):
     df = pd.DataFrame(df)
 
-    df = df[[
-            'egid', 'annee', 'indice', 'sre',
-            'adresse', 'npa', 'commune', 'destination',
-            'agent_energetique_1', 'quantite_agent_energetique_1', 'unite_agent_energetique_1',
-            'agent_energetique_2', 'quantite_agent_energetique_2', 'unite_agent_energetique_2',
-            'agent_energetique_3', 'quantite_agent_energetique_3', 'unite_agent_energetique_3',
-            'date_debut_periode', 'date_fin_periode', 'date_saisie',
-            'indice_moy2', 'annees_concernees_moy_2', 'indice_moy3', 'annees_concernees_moy_3',
-            'id_concessionnaire', 'nbre_preneur',
-            ]].sort_values(by=['egid', 'annee'])
+    df = df[
+        [
+            "egid",
+            "annee",
+            "indice",
+            "sre",
+            "adresse",
+            "npa",
+            "commune",
+            "destination",
+            "agent_energetique_1",
+            "quantite_agent_energetique_1",
+            "unite_agent_energetique_1",
+            "agent_energetique_2",
+            "quantite_agent_energetique_2",
+            "unite_agent_energetique_2",
+            "agent_energetique_3",
+            "quantite_agent_energetique_3",
+            "unite_agent_energetique_3",
+            "date_debut_periode",
+            "date_fin_periode",
+            "date_saisie",
+            "indice_moy2",
+            "annees_concernees_moy_2",
+            "indice_moy3",
+            "annees_concernees_moy_3",
+            "id_concessionnaire",
+            "nbre_preneur",
+        ]
+    ].sort_values(by=["egid", "annee"])
 
     st.dataframe(df)
 
+
 @st.cache_data
 def get_adresses_egid():
-    conn = sqlite3.connect('adresses_egid.db')
+    conn = sqlite3.connect("adresses_egid.db")
     c = conn.cursor()
-    df_adresses_egid = pd.read_sql_query("SELECT * FROM adresses_egid ORDER BY adresse", conn)
+    df_adresses_egid = pd.read_sql_query(
+        "SELECT * FROM adresses_egid ORDER BY adresse", conn
+    )
     conn.commit()
     conn.close()
     return df_adresses_egid
 
+
 @st.cache_data
 def create_barplot(data_df):
     df_barplot = pd.DataFrame(data_df)
-    df_barplot = df_barplot[['adresse', 'egid', 'annee', 'indice']].sort_values(by=['annee', 'adresse', 'egid'])
-    
+    df_barplot = df_barplot[["adresse", "egid", "annee", "indice"]].sort_values(
+        by=["annee", "adresse", "egid"]
+    )
+
     # Check if annee has all the years between 2011 and current year
     current_year = datetime.now().year
     years = list(range(2011, current_year + 1))
-    adresses_egid = df_barplot[['adresse', 'egid']].drop_duplicates().values.tolist()
-    
+    adresses_egid = df_barplot[["adresse", "egid"]].drop_duplicates().values.tolist()
+
     # Create a list to store new rows
     new_rows = []
-    
+
     for adresse, egid in adresses_egid:
         for year in years:
-            if year not in df_barplot[(df_barplot['adresse'] == adresse) & (df_barplot['egid'] == egid)]['annee'].unique():
-                new_rows.append({'adresse': adresse, 'egid': egid, 'annee': year, 'indice': 0})
-    
+            if (
+                year
+                not in df_barplot[
+                    (df_barplot["adresse"] == adresse) & (df_barplot["egid"] == egid)
+                ]["annee"].unique()
+            ):
+                new_rows.append(
+                    {"adresse": adresse, "egid": egid, "annee": year, "indice": 0}
+                )
+
     # Use concat to add new rows
     if new_rows:
         df_new_rows = pd.DataFrame(new_rows)
         df_barplot = pd.concat([df_barplot, df_new_rows], ignore_index=True)
-    
-    df_barplot = df_barplot.sort_values(by=['annee', 'adresse', 'egid'])
-    
+
+    df_barplot = df_barplot.sort_values(by=["annee", "adresse", "egid"])
+
     # Create a new column for legend
-    df_barplot['adresse_egid'] = df_barplot.apply(lambda row: f"{row['adresse']} - {row['egid']}", axis=1)
-    
-    fig = px.bar(df_barplot, x='annee', y='indice', color='adresse_egid', barmode='group',
-                 labels={'annee': 'Année', 'indice': 'Indice [MJ/m²]'},
-                 title='Indice par Année et Adresse')
-    
+    df_barplot["adresse_egid"] = df_barplot.apply(
+        lambda row: f"{row['adresse']} - {row['egid']}", axis=1
+    )
+
+    fig = px.bar(
+        df_barplot,
+        x="annee",
+        y="indice",
+        color="adresse_egid",
+        barmode="group",
+        labels={"annee": "Année", "indice": "Indice [MJ/m²]"},
+        title="Indice par Année et Adresse",
+    )
+
     # Customize the layout
     fig.update_layout(
-        xaxis_title='Année',
-        yaxis_title='Indice [MJ/m²]',
-        legend_title='Adresse - EGID',
-        xaxis={'type': 'category'}  # Treat year as categorical
+        xaxis_title="Année",
+        yaxis_title="Indice [MJ/m²]",
+        legend_title="Adresse - EGID",
+        xaxis={"type": "category"},  # Treat year as categorical
     )
-    
+
     return fig
--- sections/helpers/validation_saisie.py
+++ sections/helpers/validation_saisie.py
@@ -1,34 +1,39 @@
 import streamlit as st
 import numpy as np
 
+
 def validate_input(name, variable, unit):
-    if (variable.isnumeric() or variable.replace('.', '', 1).isnumeric()):
+    if variable.isnumeric() or variable.replace(".", "", 1).isnumeric():
         st.text(f"{name} {variable} {unit}")
     else:
         st.warning(f"{name} doit être un chiffre")
 
+
 def validate_input_affectation(name, variable, unite, sre_renovation_m2):
     try:
-        variable = float(variable.replace(',', '.', 1))
+        variable = float(variable.replace(",", ".", 1))
         if 0 <= variable <= 100:
-            st.text(f"{name} {variable} {unite} → {round(variable * float(sre_renovation_m2) / 100, 2)} m²")
+            st.text(
+                f"{name} {variable} {unite} → {round(variable * float(sre_renovation_m2) / 100, 2)} m²"
+            )
         else:
             st.warning(f"Valeur doit être comprise entre 0 et 100")
     except ValueError:
         st.warning(f"{name} doit être un chiffre")
         variable = 0
 
+
 def validate_agent_energetique_input(name, value, unit):
     if value is None or value == "":
         st.text(f"{name} doit être un chiffre")
         return 0
-    
+
     try:
         if isinstance(value, (int, float, np.float64)):
             value = float(value)
         else:
-            value = float(str(value).replace(',', '.', 1))
-        
+            value = float(str(value).replace(",", ".", 1))
+
         if value > 0:
             st.text(f"{name} {value} {unit}")
             return value
@@ -42,7 +47,7 @@ def validate_agent_energetique_input(name, value, unit):
 
 def validate_energie_input(name, variable, unit1, unit2):
     try:
-        variable = float(variable.replace(',', '.', 1))
+        variable = float(variable.replace(",", ".", 1))
         if variable > 0:
             st.text(f"{name} {variable} {unit1} → {round((variable * 3.6),2)} {unit2}")
         else:

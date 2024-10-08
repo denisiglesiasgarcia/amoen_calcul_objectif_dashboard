7a0577e4e29d3e62d02e2879874065bde9a321af
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-10-08 18:26:35 +0200

    fix latex texte et formule

--- sections/helpers/note_calcul_main.py
+++ sections/helpers/note_calcul_main.py
@@ -85,9 +85,9 @@ from sections.helpers.note_calcul.constantes import (
 )
 
 from sections.helpers.note_calcul.latex import (
-    make_latex_formula_facteur_ponderation_moyen_texte,
     make_latex_formula_facteur_ponderation_moyen,
 )
+
 
 def fonction_note_calcul(data_site, df_meteo_tre200d0):
     """
@@ -394,13 +394,31 @@ def fonction_note_calcul(data_site, df_meteo_tre200d0):
     )
 
     # latex text
-    formula_facteur_ponderation_moyen_texte = (
-        make_latex_formula_facteur_ponderation_moyen_texte(),
-    )
+    formula_facteur_ponderation_moyen_texte = r"facteur\_ponderation\_moyen = \frac{{(agent\_energetique\_mazout\_somme\_mj \times FACTEUR\_PONDERATION\_MAZOUT + \
+                agent\_energetique\_gaz\_naturel\_somme\_mj \times FACTEUR\_PONDERATION\_GAZ\_NATUREL + \
+                agent\_energetique\_bois\_buches\_dur\_somme\_mj \times FACTEUR\_PONDERATION\_BOIS\_BUCHES\_DUR + \
+                agent\_energetique\_bois\_buches\_tendre\_somme\_mj \times FACTEUR\_PONDERATION\_BOIS\_BUCHES\_TENDRE + \
+                agent\_energetique\_pellets\_somme\_mj \times FACTEUR\_PONDERATION\_PELLETS + \
+                agent\_energetique\_plaquettes\_somme\_mj \times FACTEUR\_PONDERATION\_PLAQUETTES + \
+                agent\_energetique\_cad\_somme\_mj \times FACTEUR\_PONDERATION\_CAD + \
+                agent\_energetique\_electricite\_pac\_somme\_mj \times FACTEUR\_PONDERATION\_ELECTRICITE\_PAC + \
+                agent\_energetique\_electricite\_directe\_somme\_mj \times FACTEUR\_PONDERATION\_ELECTRICITE\_DIRECTE + \
+                agent\_energetique\_autre\_somme\_mj \times FACTEUR\_PONDERATION\_AUTRE)}}{{(agent\_energetique\_somme\_kwh \times 3.6)}}"
 
     # latex formula
     formula_facteur_ponderation_moyen = make_latex_formula_facteur_ponderation_moyen(
-        data_site,
+        data_site["agent_energetique_ef_mazout_somme_mj"],
+        data_site["agent_energetique_ef_gaz_naturel_somme_mj"],
+        data_site["agent_energetique_ef_bois_buches_dur_somme_mj"],
+        data_site["agent_energetique_ef_bois_buches_tendre_somme_mj"],
+        data_site["agent_energetique_ef_pellets_somme_mj"],
+        data_site["agent_energetique_ef_plaquettes_somme_mj"],
+        data_site["agent_energetique_ef_cad_somme_mj"],
+        data_site["agent_energetique_ef_electricite_pac_somme_mj"],
+        data_site["agent_energetique_ef_electricite_directe_somme_mj"],
+        data_site["agent_energetique_ef_autre_somme_mj"],
+        data_site["agent_energetique_ef_somme_kwh"],
+        data_site["facteur_ponderation_moyen"],
         FACTEUR_PONDERATION_MAZOUT,
         FACTEUR_PONDERATION_GAZ_NATUREL,
         FACTEUR_PONDERATION_BOIS_BUCHES_DUR,

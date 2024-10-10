7cfe21c9c8cad7e1be7f3ae50396f14115f0fb31
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-10-10 10:42:11 +0200

    Update create_dataframe_meteo.py

--- sections/helpers/note_calcul/create_dataframe_meteo.py
+++ sections/helpers/note_calcul/create_dataframe_meteo.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 
 def make_dataframe_df_meteo_note_calcul(periode_start, periode_end, df_meteo_tre200d0):
+    periode_start = pd.to_datetime(periode_start)
+    periode_end = pd.to_datetime(periode_end)
     df_meteo_note_calcul = df_meteo_tre200d0[
         (df_meteo_tre200d0["time"] >= periode_start)
         & (df_meteo_tre200d0["time"] <= periode_end)

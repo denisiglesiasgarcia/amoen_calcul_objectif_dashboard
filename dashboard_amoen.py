b9ada1431de7cd24a1f59b5e2d6d802663af25fd
denisiglesiasgarcia <71026758+denisiglesiasgarcia@users.noreply.github.com>
2024-10-25 14:16:51 +0200

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -276,7 +276,7 @@ if st.session_state["authentication_status"]:
         except Exception as e:
             st.error(e)
     authenticator.logout(location="sidebar")
-    st.sidebar.write("You are logged as: ", st.session_state["username"])
+    st.sidebar.write("utilisateur: ", st.session_state["username"])
 
     # dashboard
     st.title("Calcul de l'atteinte des objectifs AMOén selon méthodologie")

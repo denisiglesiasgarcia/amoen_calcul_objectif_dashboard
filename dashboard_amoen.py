608cfb01c910611c69d2614889b0dce13f068389
Iglesias Garcia Denis (HES) <denis.iglesias-garcia@hesge.ch>
2024-12-13 15:50:20 +0100

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -451,8 +451,6 @@ if st.session_state["authentication_status"]:
 
             # Validation somme des pourcentages
             try:
-                # Get and print each value to verify what we're receiving
-
                 val1 = get_rounded_float(
                     st.session_state["data_site"],
                     "repartition_energie_finale_partie_renovee_chauffage",
@@ -473,8 +471,7 @@ if st.session_state["authentication_status"]:
                 repartition_ef_somme_avertissement = (
                     float(val1) + float(val2) + float(val3) + float(val4)
                 )
-                st.write(val1, val2, val3, val4)
-                if abs(repartition_ef_somme_avertissement - 100) > 0.01:  # Use small tolerance for float comparison
+                if repartition_ef_somme_avertissement != 100:
                     st.warning(
                         f"La somme des pourcentages de répartition de l'énergie finale doit être égale à 100% ({repartition_ef_somme_avertissement:.2f}%)"
                     )

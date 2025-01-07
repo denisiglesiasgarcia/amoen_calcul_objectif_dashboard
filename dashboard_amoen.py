0cf964bbbe7702e5bc2176f6649aedc5b6e568b7
Iglesias Garcia Denis (HES) <denis.iglesias-garcia@hesge.ch>
2025-01-07 09:52:15 +0100

    Update dashboard_amoen.py

--- dashboard_amoen.py
+++ dashboard_amoen.py
@@ -470,7 +470,7 @@ if st.session_state["authentication_status"]:
                     "repartition_energie_finale_partie_surelevee_ecs",
                 )
                 # Convert to float before adding
-                repartition_ef_somme_avertissement = round(float(val1) + float(val2) + float(val3) + float(val4), 2)
+                repartition_ef_somme_avertissement = round(val1 + val2 + val3 + val4, 2)
                 if repartition_ef_somme_avertissement != 100.0:
                     st.warning(
                         f"La somme des pourcentages de répartition de l'énergie finale doit être égale à 100% ({repartition_ef_somme_avertissement:.2f}%)"
